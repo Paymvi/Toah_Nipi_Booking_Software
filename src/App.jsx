b5982e216492676ca9a84cbc81dc840da478d988
Chelsea Alysson Ongjoco <chelsalysongjoco@yahoo.com>
2026-06-17 14:53:23 -0400

    Extract Calendar View CSS into its own file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import "./styles/shared.css";
 import "./styles/Dashboard.css";
+import "./styles/CalendarView.css";
 import Dashboard from "./pages/Dashboard";
 import StaffAuthGate from "./auth/StaffAuthGate";
 
