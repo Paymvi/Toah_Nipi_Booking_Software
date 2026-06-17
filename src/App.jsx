078da37b31f859f1c5627dab23059eb76a66f654
Chelsea Alysson Ongjoco <chelsalysongjoco@yahoo.com>
2026-06-17 15:02:30 -0400

    Extract Inquiry Pipeline CSS into its own file

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import "./styles/shared.css";
 import "./styles/Dashboard.css";
 import "./styles/CalendarView.css";
+import "./styles/InquiryPipeline.css";
 import Dashboard from "./pages/Dashboard";
 import StaffAuthGate from "./auth/StaffAuthGate";
 
